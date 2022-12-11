65c25c52b920319ae4d58d16053db179e6c293f6
nbogie <neillbogie@googlemail.com>
2022-12-11 01:55:44 +0000

    slots can be clicked, disabling them

--- src/components/tictactoe/TicTacToeDemo.tsx
+++ src/components/tictactoe/TicTacToeDemo.tsx
@@ -2,19 +2,20 @@ import { OrbitControls, Stage } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { useEffect, useState } from 'react';
 
-function posForSlot(i: number): [number, number, number] {
-    return [i % 3, 0, Math.floor(i / 3)]
-}
-
-function colourForSlot(i: number): string {
-    return i % 2 === 0 ? "gray" : "white"
-
-}
-
 export function TicTacToeDemo() {
     const slots = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     const [selectedSlot, setSelectedSlot] = useState<number | null>(null);
+    const [disabledSlots, setDisabledSlots] = useState<number[]>([]);
 
+    function handleSlotClicked(slot: number) {
+        setDisabledSlots(prevs => {
+            if (!prevs.includes(slot)) {
+                return [...prevs, slot];
+            } else {
+                return prevs;
+            }
+        })
+    }
     return (
         <div className="demo-container-with-side">
             <div>
@@ -33,7 +34,9 @@ export function TicTacToeDemo() {
                                     isSelected={selectedSlot === slot}
                                     setSelected={setSelectedSlot}
                                     deselect={slot => (slot === selectedSlot) && setSelectedSlot(null)}
+                                    onClick={handleSlotClicked}
                                     slot={slot}
+                                    isDisabled={disabledSlots.includes(slot)}
                                 />)
                             )}
                         </group>
@@ -49,23 +52,28 @@ interface BoardTileProps {
     isSelected: boolean;
     setSelected: (slot: number) => void;
     deselect: (slot: number) => void;
+    onClick: (slot: number) => void;
+    isDisabled: boolean;
+
 }
 
 function BoardTile(props: BoardTileProps) {
-    const { slot, isSelected, setSelected, deselect } = props;
+    const { slot, isSelected, setSelected, deselect, onClick, isDisabled } = props;
     return (
         <group
             onPointerOver={() => setSelected(slot)}
-            onPointerOut={() => deselect(slot)}>
+            onPointerOut={() => deselect(slot)}
+            onClick={() => onClick(slot)}
+        >
 
             <mesh position={posForSlot(slot)} scale={[0.8, 1, 0.8]}>
-                <meshStandardMaterial color={colourForSlot(slot)} />
+                <meshStandardMaterial color={colourForSlot(slot, isDisabled)} />
                 <boxGeometry args={[1, 0.25, 1]} />
             </mesh>
 
             <mesh position={posForSlot(slot)}>
                 <meshStandardMaterial
-                    color={isSelected ? "magenta" : colourForSlot(slot)}
+                    color={isSelected ? "magenta" : "red"}
                     transparent={true}
                     opacity={isSelected ? 1 : 0}
                 />
@@ -74,4 +82,17 @@ function BoardTile(props: BoardTileProps) {
         </group>
 
     )
+}
+
+
+function posForSlot(i: number): [number, number, number] {
+    return [i % 3, 0, Math.floor(i / 3)]
+}
+
+function colourForSlot(i: number, isDisabled: boolean): string {
+    if (isDisabled) {
+        return i % 2 === 0 ? "gray" : "white"
+    }
+    return i % 2 === 0 ? "yellow" : "dodgerblue"
+
 }
