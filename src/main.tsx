fff43aae67d97564bb4d6551e7cde45cae4620ed
nbogie <neillbogie@googlemail.com>
2022-12-12 10:50:35 +0000

    use hashrouter for no-config on netlify

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { MyRoutes } from './MyRoutes'
 
 import './components/App.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <MyRoutes />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
 
