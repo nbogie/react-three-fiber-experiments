8812f8e52dad7a9b395589271a51362fbb137ae1
nbogie <neillbogie@googlemail.com>
2022-12-14 00:36:24 +0000

    add MeshReflectiveMaterial to postproc demo

--- src/components/PostProcessingDemo.tsx
+++ src/components/PostProcessingDemo.tsx
@@ -1,4 +1,4 @@
-import { Float, OrbitControls, Stage, Stars, Text } from '@react-three/drei';
+import { Float, OrbitControls, Stage, Stars, Text, MeshReflectorMaterial } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Bloom, DepthOfField, EffectComposer, Glitch, Noise, Vignette } from '@react-three/postprocessing';
 import React, { useMemo, useState } from 'react';
@@ -25,9 +25,35 @@ export function PostProcessingDemo() {
                 <Canvas>
                     <OrbitControls autoRotate={false} />
                     <Stage>
-                        <CubesSet numCubes={opt * 10} />
+                        <CubesSet numCubes={opt * 3} />
                     </Stage>
                     <Stars radius={100} depth={50} count={2000} factor={4} saturation={0} speed={0} />
+
+                    <mesh rotation-x={-Math.PI / 2} position-y={-5}>
+                        <circleGeometry args={[15, 32]} />
+                        <MeshReflectorMaterial
+                            blur={[0, 0]} // Blur ground reflections (width, heigt), 0 skips blur
+                            mixBlur={0.4} // How much blur mixes with surface roughness (default = 1)
+                            mixStrength={0.7} // Strength of the reflections
+                            mixContrast={0.4} // Contrast of the reflections
+                            resolution={512} // Off-buffer resolution, lower=faster, higher=better quality, slower
+                            mirror={1} // Mirror environment, 0 = texture colors, 1 = pick up env colors
+                            // depthScale={0} // Scale the depth factor (0 = no depth, default = 0)
+                            // minDepthThreshold={0.9} // Lower edge for the depthTexture interpolation (default = 0)
+                            // maxDepthThreshold={1} // Upper edge for the depthTexture interpolation (default = 0)
+                            depthToBlurRatioBias={0.25} // Adds a bias factor to the depthTexture before calculating the blur amount [blurFactor = blurTexture * (depthTexture + bias)]. It accepts values between 0 and 1, default is 0.25. An amount > 0 of bias makes sure that the blurTexture is not too sharp because of the multiplication with the depthTexture
+                            distortion={1} // Amount of distortion based on the distortionMap texture
+                            // distortionMap={distortionTexture} // The red channel of this texture is used as the distortion map. Default is null
+                            /*
+                            0 = no debug
+                            1 = depth channel
+                            2 = base channel
+                            3 = distortion channel
+                            4 = lod channel (based on the roughness)
+                            */
+                            reflectorOffset={0.2} // Offsets the virtual camera that projects the reflection. Useful when the reflective surface is some distance from the object's origin (default = 0)
+                        />
+                    </mesh>
                     {/* <Sparkles count={500} scale={10} size={5} speed={0.3} noise={0} /> */}
                     <EffectComposer>
                         <DepthOfField focusDistance={0} focalLength={0.02} bokehScale={2} height={480} />
@@ -36,7 +62,6 @@ export function PostProcessingDemo() {
                         {/* https://docs.pmnd.rs/react-postprocessing/effects/glitch */}
                         <Glitch delay={new Vector2(5, 15)} />
                         <Vignette eskil={false} offset={0.1} darkness={1.1} />
-
                     </EffectComposer>
 
                 </Canvas>
@@ -81,7 +106,7 @@ interface IParticle {
 }
 
 function createParticle(): IParticle {
-    const distFromCentre: number = Math.sqrt(Math.abs(randFloatSpread(1))) * 10;
+    const distFromCentre: number = Math.sqrt(Math.abs(randFloatSpread(1))) * 13;
     return {
         pos: new Vector3(0, 1, 0).randomDirection().multiplyScalar(distFromCentre),
         vel: new Vector3(0, 0, 0),
