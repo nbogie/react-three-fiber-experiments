2f68b52c390f8232d22b21a70c81e65ab16164f1
nbogie <neillbogie@googlemail.com>
2023-01-04 19:27:17 +0000

    add naive chase camera

--- src/components/cityDriveSimplest/CityDriveSimplestDemo.tsx
+++ src/components/cityDriveSimplest/CityDriveSimplestDemo.tsx
@@ -1,9 +1,8 @@
-import { OrbitControls, Stage } from '@react-three/drei';
-import { Canvas } from '@react-three/fiber';
-import { Bloom, EffectComposer } from '@react-three/postprocessing';
-import { useMemo, useState } from 'react';
+import { PerspectiveCamera, Plane } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef, useState } from 'react';
 import { Vector3 } from 'three';
-import { lerp, randFloat, randFloatSpread } from 'three/src/math/MathUtils';
+import { randFloat, randFloatSpread } from 'three/src/math/MathUtils';
 
 const palette = [
     "#5e3929",
@@ -12,28 +11,58 @@ const palette = [
     "#dee8be",
     "#fcf7d3"
 ];
+
 function CityDriveSimplestDemo() {
 
     return (
         <div className="canvas-container">
-            <Canvas>
-                <Stage>
-                    <CityDriveSimplest />
-                </Stage>
+            <Canvas camera={{ position: [0, 1, -2], fov: 50 }}>
 
-                <OrbitControls autoRotate={true} autoRotateSpeed={0.5} rotateSpeed={1} />
+                <CityDriveSimplest />
+                {/* <OrbitControls autoRotate={true} autoRotateSpeed={0.5} rotateSpeed={1} /> */}
             </Canvas>
         </div >
     )
 }
 
+
 export default CityDriveSimplestDemo;
 
 
+interface Car {
+    position: Vector3;
+    speed: number;
+}
+
 export function CityDriveSimplest() {
 
+    const cameraRef = useRef<THREE.PerspectiveCamera>(null!);
+
+    const [car, setCar] = useState<Car>({ position: new Vector3(0, 0, 0), speed: 0.01 });
+
+    useFrame(() => {
+        //Note: changing react-managed state in useFrame is NOT recommended for performance reasons - it will lead to too much re-rendering
+        setCar((prev) => ({ ...prev, position: prev.position.clone().add(new Vector3(0, 0, -prev.speed)) }));
+        cameraRef.current.position.z += -car.speed;
+        cameraRef.current.lookAt(new Vector3(0, 0, cameraRef.current.position.z - 10))
+
+    })
+
     return (
-        <Buildings numBuildings={100} />
+        <group>
+
+            <PerspectiveCamera
+                makeDefault={true}
+                position={[0, 2, 10]}
+                near={0.1}
+                far={100}
+                ref={cameraRef} />
+
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[10, 10, 5]} intensity={1} />
+            <Buildings numBuildings={100} />
+
+        </group>
 
     );
 }
@@ -77,9 +106,10 @@ function Buildings(props: BuildingsProps) {
     return (
         <group>
             {
-                buildings.map((b, i) => < BuildingView data={b} />)
+                buildings.map((b, i) => < BuildingView key={i} data={b} />)
             }
-
+            <Road />
+            <Ground />
         </group>
     );
 
@@ -107,4 +137,20 @@ export function collect<T>(num: number, createFn: (n: number) => T): T[] {
 
 function pick<T>(arr: T[]): T {
     return arr[Math.floor(Math.random() * arr.length)];
+}
+function Road(): JSX.Element {
+    return (
+        <Plane args={[10, 10]} rotation-x={-Math.PI / 2} >
+            <meshStandardMaterial color={"green"} />
+
+        </Plane>
+    )
+}
+function Ground(): JSX.Element {
+    return (
+
+        <Plane args={[1, 10]} position={[0, 0.001, 0]} rotation-x={-Math.PI / 2}>
+            <meshStandardMaterial color={"#333"} />
+        </Plane>
+    )
 }
